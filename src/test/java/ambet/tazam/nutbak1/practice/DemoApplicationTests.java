d82eff7f8e2da68710f7804da784ebfdccd12081
ambet <ambetazam@gmail.com>
2026-09-23 12:20:31 -0400

    feat: Create Spring Boot Project

---
+++ src/test/java/ambet/tazam/nutbak1/practice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ambet.tazam.nutbak1.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
